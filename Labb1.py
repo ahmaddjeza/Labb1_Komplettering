5145f27079e39699ca2cf78e439f1f4c79457f5a
ahmaddjeza <djezaahmad@gmail.com>
2025-04-28 08:02:16 +0200

    Skapar en metod som läser in DNA fil och returnerar en dictionary med namn och sekvenser.

--- Labb1.py
+++ Labb1.py
@@ -1,1 +1,19 @@
 import matplotlib.pyplot as plt
+
+# 1. Läser DNA-filen och returnerar en dictionary med namn och sekvenser
+def read_dna_file(filename):
+    sequences = {}
+    current_name = ""
+    
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()  # Tar bort radbrytningar och mellanslag
+            
+            if line.startswith('>'):  # Om raden börjar med '>' är det ett nytt namn
+                current_name = line[1:]  # Sparar namnet (utan '>')
+                sequences[current_name] = ""  # Skapar en tom sekvens
+            else:
+                sequences[current_name] += line.upper()  # Lägger till sekvensen
+    
+    return sequences
+
